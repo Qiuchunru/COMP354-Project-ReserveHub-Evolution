51a85c5869a63554505b713920b1423a1e00f6d7
DexComm69 <muhammad36163@gmail.com>
2026-05-08 15:46:45 +0800

    Optimize repository structure, add collaboration docs, and clean up unnecessary files

--- sql/update_images.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE restaurants ADD COLUMN image_url VARCHAR(255);
-
--- Update all restaurants to have a realistic food image URL based on their ID to keep it consistent
-UPDATE restaurants SET image_url = CONCAT('https://loremflickr.com/600/400/food,malaysia?random=', id);
