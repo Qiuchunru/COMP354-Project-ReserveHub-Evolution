05139f3e8b6cc1dd772fc96b7ca85816cfe741bf
sim246 <simonadragomir2001@gmail.com>
2026-07-05 01:54:19 -0400

    translation for contact

--- js/localization.js
+++ js/localization.js
@@ -1,15 +1,15 @@
-async function loadLanguage(locale) {
+async function loadLanguage(json) {
   try {
-    const response = await fetch(`../json/${locale}.json`);
+    const response = await fetch(json);
     if (!response.ok) {
       throw new Error(`Status: ${response.status}`);
     }
     const translations = await response.json();
-
     document.querySelectorAll('[localization-key]').forEach(element => {
       const key = element.getAttribute('localization-key');
+      console.log(key, translations[key]);
       if (translations[key]) {
-        element.innerText = translations[key];
+        element.textContent = translations[key];
       }
     });
   } catch (error) {
@@ -17,8 +17,16 @@ async function loadLanguage(locale) {
   }
 }
 
-const languageSelect = document.getElementById("language-localization");
+const indexElements = document.querySelectorAll(".index-language-localization");
+indexElements.forEach(element => {
+    element.addEventListener("change", () => {
+        loadLanguage(`../json/index-${element.value}.json`);
+    })
+});
 
-languageSelect.addEventListener("change", () => {
-    loadLanguage(languageSelect.value);
+const contactElements = document.querySelectorAll(".contact-language-localization");
+contactElements.forEach(element => {
+    element.addEventListener("change", () => {
+        loadLanguage(`../json/contact-${element.value}.json`);
+    })
 });
