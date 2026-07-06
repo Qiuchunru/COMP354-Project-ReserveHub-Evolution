1ad9bf5e2c25e8c68a7b5db719c27df3b0adbecf
sim246 <simonadragomir2001@gmail.com>
2026-07-06 02:17:32 -0400

    small fixes

--- js/localization.js
+++ js/localization.js
@@ -1,25 +1,30 @@
-async function loadLanguage(json) {
+async function loadLanguage(language) {
   try {
-    const response = await fetch(json);
+    
+    document.documentElement.lang = language;
+    const response = await fetch(`../json/${language}.json`);
+
     if (!response.ok) {
       throw new Error(`Status: ${response.status}`);
     }
+
     const translations = await response.json();
-    document.querySelectorAll('[localization-key]').forEach(element => {
-      const key = element.getAttribute('localization-key');
+    document.querySelectorAll("[localization-key]").forEach(element => {
+      const key = element.getAttribute("localization-key");
       if (translations[key]) {
         element.textContent = translations[key];
       }
     });
+
   } catch (error) {
-    console.error('Failed to load translations! ', error);
+    console.error("Failed to load translations!", error);
   }
 }
 
 const elements = document.querySelectorAll(".language-localization");
 elements.forEach(element => {
-    element.addEventListener("change", () => {
-      let language = element.value;
-      loadLanguage(`../json/${language}.json`);
-    })
+  element.addEventListener("change", () => {
+    const language = element.value;
+    loadLanguage(language);
+  });
 });
